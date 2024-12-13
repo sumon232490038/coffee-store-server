f518361afba7069cfc020539c2bc08eacd44cc82
sumon232 <sumonmia4526@gmail.com>
2024-12-13 08:51:09 +0600

    server site to database data post is done

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.xhl2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,20 +19,19 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
-    app.get("/", (req, res) => {
-      res.send("bangadesh");
+    const database = client.db("coffeeBD");
+    const coffeeDatabase = database.collection("coffee");
+    app.post("/addCoffee", async (req, res) => {
+      const coffee = req.body;
+      // console.log(coffee);
+      const result = await coffeeDatabase.insertOne(coffee);
+      res.send(result);
     });
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
