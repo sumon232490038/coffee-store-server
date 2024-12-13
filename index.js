450f1e5e8179d5ecce9313c5bfc58ae92e28b0c7
sumon232 <sumonmia4526@gmail.com>
2024-12-13 11:34:39 +0600

    Alhamdulillah in this server  site coffee item adding, updating,and showing details is done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
@@ -21,10 +21,57 @@ async function run() {
   try {
     const database = client.db("coffeeBD");
     const coffeeDatabase = database.collection("coffee");
+
+    app.get("/coffee", async (req, res) => {
+      const find = coffeeDatabase.find();
+      const result = await find.toArray();
+      res.send(result);
+      console.log(result);
+    });
+
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeDatabase.findOne(query);
+      res.send(result);
+    });
+
     app.post("/addCoffee", async (req, res) => {
       const coffee = req.body;
       // console.log(coffee);
       const result = await coffeeDatabase.insertOne(coffee);
+      res.send(result);
+    });
+
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeDatabase.deleteOne(query);
+      res.send(result);
+      console.log(result);
+    });
+
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const coffee = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateCoffee = {
+        $set: {
+          name: coffee.name,
+          chef: coffee.chef,
+          details: coffee.details,
+          supplier: coffee.supplier,
+          price: coffee.price,
+          photo: coffee.photo,
+          category: coffee.category,
+        },
+      };
+      const result = await coffeeDatabase.updateOne(
+        query,
+        updateCoffee,
+        options
+      );
       res.send(result);
     });
 
